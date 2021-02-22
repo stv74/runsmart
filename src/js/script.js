9a4e69dafcc7a834a8372f531492a8ea61f7dc10
Taras Samoilov <tarsam74@gmail.com>
2021-02-22 16:48:51 +0200

    Started work on the Owl Carousel

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+/* $(document).ready(function(){
     $('.carousel__inner').slick({           
       speed: 1200,  
       prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
@@ -12,5 +12,30 @@ $(document).ready(function(){
           }
         }
       ]
+    });
+  }); */
+
+  $(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+      loop:true,
+      margin:10,        
+      items:1,
+      dots:false,
+      nav:true,
+      navText:[
+        '<div class="owl-prev"><img src="icons/left.svg"></div>', 
+        '<div class="owl-next"><img src="icons/right.svg"></div>'
+      ],                  
+      responsive:{
+          0:{
+              items:1
+          },
+          600:{
+              items:3
+          },
+          1000:{
+              items:5
+          }
+      }
     });
   });

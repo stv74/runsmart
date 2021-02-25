d2275d1d08c5504f6809ea7a60632c97e301b4c6
Taras Samoilov <tarsam74@gmail.com>
2021-02-25 16:38:39 +0200

    Separated the sliders Slick-slider and Owl-carousel in the code. I continue to study Owl-carousel

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,10 @@
+// Slick-slider
 /* $(document).ready(function(){
     $('.carousel__inner').slick({           
       speed: 1200,  
       prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
       nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+      dots: true,
       responsive: [
         {
           breakpoint: 992,
@@ -15,11 +17,18 @@
     });
   }); */
 
+// Owl-carousel
   $(document).ready(function(){
     $(".owl-carousel").owlCarousel({
       loop:true,
       margin:10,        
-      items:1,                        
+      items:1,      
+      navSpeed:1200,
+      dotsSpeed:1200,
+      autoplay:true,
+      autoplayTimeout:3000,
+      autoplaySpeed:1200,
+      autoplayHoverPause:true,                        
       responsive:{
         0:{
           dots:true,

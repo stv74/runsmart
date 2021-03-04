eaad87e4686826690e7f461b8a2753fee20f5f58
Taras Samoilov <tarsam74@gmail.com>
2021-03-04 16:26:03 +0200

    Finished work on section Catalog

--- src/js/script.js
+++ src/js/script.js
@@ -17,8 +17,8 @@
     });
   }); */
 
-// Owl-carousel
   $(document).ready(function(){
+    // Owl-carousel
     $(".owl-carousel").owlCarousel({
       loop:true,
       margin:10,        
@@ -44,4 +44,25 @@
         }
       }
     });
+
+    // Tab switcher
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+      $(this)
+        .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+        .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+
+    // Card switcher
+    function toggleSlide(item) {
+      $(item).each(function(i) {
+        $(this).on('click', function(e) {
+          e.preventDefault();
+          $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+          $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+        })
+      });
+    };
+
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
   });

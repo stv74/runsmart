2d54c4c806debace1ca0e31839b77735e22aad67
Taras Samoilov <tarsam74@gmail.com>
2021-04-01 16:42:35 +0300

    Added PHP-mailer

--- src/js/script.js
+++ src/js/script.js
@@ -116,4 +116,20 @@
 
     // MaskedInput
     $('input[name=phone]').mask("+7 (999) 999-99-99");
+
+    //PHP-mailer
+    $('form').submit(function(e) {
+      e.preventDefault();
+      $.ajax({
+        type: "POST",
+        url: "mailer/smart.php",
+        data: $(this).serialize()
+      }).done(function() {
+        $(this).find("input").val("");
+        $('#consultation, #order').fadeOut();
+        $('.overlay, #thanks').fadeIn('slow');
+        $('form').trigger('reset');
+      });
+      return false;
+    });
   });

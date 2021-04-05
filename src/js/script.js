c28c072b37d5b87c522156b5832125c775fbad36
Taras Samoilov <tarsam74@gmail.com>
2021-04-05 15:51:29 +0300

    Added smooth scroll and pageup

--- src/js/script.js
+++ src/js/script.js
@@ -135,4 +135,19 @@
       });
       return false;
     });
+
+    // Smooth scroll and pageup
+    $(window).scroll(function() {
+      if ($(this).scrollTop() > 1600) {
+        $('.pageup').fadeIn();
+      } else {
+        $('.pageup').fadeOut();
+      }      
+    });
+
+    $("a[href^='#']").click(function(){
+      var _href = $(this).attr("href");
+      $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+      return false;
+    });  
   });

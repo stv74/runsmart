295e47e43bbe6a586a187012fa9de52ee8e91e37
Taras Samoilov <tarsam74@gmail.com>
2021-04-05 16:46:28 +0300

    Added animation in section Advantages

--- src/js/script.js
+++ src/js/script.js
@@ -149,5 +149,7 @@
       var _href = $(this).attr("href");
       $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
       return false;
-    });  
+    }); 
+    
+    new WOW().init();
   });

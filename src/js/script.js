870677a2d67230de4083b1de77f270be919e46a9
Taras Samoilov <tarsam74@gmail.com>
2021-04-06 20:31:17 +0300

    Several minor bugs fixed

--- src/js/script.js
+++ src/js/script.js
@@ -145,7 +145,7 @@
       }      
     });
 
-    $("a[href^='#']").click(function(){
+    $("a[href=#up]").click(function(){
       var _href = $(this).attr("href");
       $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
       return false;
